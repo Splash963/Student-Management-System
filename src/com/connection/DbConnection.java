1fc9320d89c4b35dc67a5beb36633a84292e76ac
Theekshana Hirushan <theekshanahirushan10@gmail.com>
2026-03-05 00:04:14 +0530

    Create Database Connection class

---
+++ src/com/connection/DbConnection.java
@@ -0,0 +1,27 @@
+package com.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnection {
+
+    public static Connection connect() {
+        Connection conn = null;
+
+        try {
+            // Load the SQLite JDBC driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Establish the connection
+            conn = DriverManager.getConnection("jdbc:mysql://172.20.10.2/student_management_system", "remote_user", "Remote123@");
+            System.out.println("Connection successful!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBC Driver not found: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
+
+        return conn;
+    }
+}
